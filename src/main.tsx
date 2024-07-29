9461ca83d5c039177127ca8a5450916e4686b791
faifan143 <faisalfansa.bussiness@gmail.com>
2024-07-29 15:28:00 +0300

    new note

--- src/main.tsx
+++ src/main.tsx
@@ -64,6 +64,7 @@ import MaterialReport from "@/pages/Warehouse/MaterialReport.tsx";
 
 // Axios config
 axios.defaults.baseURL = "http://localhost:3002/";
+// new note
 // axios.defaults.baseURL = "https://dashboardbackendnew.onrender.com/";
 axios.defaults.headers.common = {
   Authorization: `bearer ${Cookies.get("access_token")}`,
