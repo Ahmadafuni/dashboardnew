202709cf70b20817b1685010689f0186f11c2845
AhmadAfyouni <ahmad.aafyouni@gmail.com>
2024-10-24 13:27:15 +0200

    fix 1

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 // config.ts
 
-export const BASE_URL = "http://localhost:3002/";
+//export const BASE_URL = "http://localhost:3002/";
 //export const BASE_URL = "https://dashboardbackendnew.onrender.com/";
-// export const BASE_URL = "https://dashboard-backend.qnv2oe.easypanel.host/";
+export const BASE_URL = "https://dashboard-backend.qnv2oe.easypanel.host/";
