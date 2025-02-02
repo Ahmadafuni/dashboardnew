241e79b15da7a0b4543e34a60bcdb76c4a266112
ahmadafyouni <ahmad.afuni91@gmail.com>
2025-02-02 11:18:50 +0100

    change to Prod database

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,9 @@
 // config.ts
 
-export const BASE_URL = "http://localhost:3002/";
-//export const BASE_URL = "https://dashboardbackendnew.onrender.com/";
+//export const BASE_URL = "http://localhost:3002/";
 
 //TestDatabase
 //export const BASE_URL = "https://dashboard-backend.qnv2oe.easypanel.host/";
 
 //ProductiondataBase
-// export const BASE_URL = "https://dashboard-backend1.qnv2oe.easypanel.host/";
+export const BASE_URL = "https://dashboard-backend1.qnv2oe.easypanel.host/";
